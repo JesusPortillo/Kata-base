d8d391d112232d7b7fedb842aa0fc75a8222b964
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-19 18:27:25 -0500

    Servicios implementados en la clase TodoServices

--- todo/src/main/java/com/sofkau/todo/services/TodoServices.java
+++ todo/src/main/java/com/sofkau/todo/services/TodoServices.java
@@ -1,4 +1,34 @@
 package com.sofkau.todo.services;
 
-public class TodoServices {
+import com.sofkau.todo.entities.Todo;
+import com.sofkau.todo.repositories.TodoRepositoryInterface;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.ArrayList;
+import java.util.Optional;
+
+public class TodoServices implements TodoServicesInterface{
+
+    @Autowired
+    private TodoRepositoryInterface repository;
+
+    @Override
+    public ArrayList<Todo> listAllTodo() {
+        return (ArrayList<Todo>) repository.findAll();
+    }
+
+    @Override
+    public Todo saveTodo(Todo todo) {
+        return repository.save(todo);
+    }
+
+    @Override
+    public void deleteTodo(Long id) {
+        repository.deleteById(id);
+    }
+
+    @Override
+    public Optional<Todo> getTodo(Long id) {
+        return repository.findById(id);
+    }
 }
--- todo/src/main/java/com/sofkau/todo/services/TodoServicesInterface.java
+++ todo/src/main/java/com/sofkau/todo/services/TodoServicesInterface.java
@@ -3,11 +3,12 @@ package com.sofkau.todo.services;
 import com.sofkau.todo.entities.Todo;
 
 import java.util.ArrayList;
+import java.util.Optional;
 
 public interface TodoServicesInterface {
 
     public ArrayList<Todo> listAllTodo();
     public Todo saveTodo(Todo todo);
     public void deleteTodo(Long id);
-    public Todo getTodo(Long id);
+    public Optional<Todo> getTodo(Long id);
 }
