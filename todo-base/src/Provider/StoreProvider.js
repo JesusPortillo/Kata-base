2fc663b023a8210f44b93b6c3d9c59f05d21bd4c
JesusPortillo <jesusdavidpv12@gmail.com>
2021-09-19 18:41:15 -0500

    Provider creado

---
+++ todo-base/src/Provider/StoreProvider.js
@@ -0,0 +1,9 @@
+
+const StoreProvider = ({ children }) => {
+    const [state, dispatch] = useReducer(reducer, initialState);
+  
+    return <Store.Provider value={{ state, dispatch }}>
+      {children}
+    </Store.Provider>
+  
+  }
